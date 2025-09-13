59fe6c42d54354ccca87191bf1621c66568bfa0f
nicomaceira <nicolas.maceira@campus.xaloc.org>
2025-09-13 18:07:23 +0200

    Add files via upload
    practica de gifthub donde se indican las tres variables y se hace la suma entre ellas

---
+++ practica_gifthub.py
@@ -0,0 +1,8 @@
+var1=4
+var2=7
+var3=5.5
+total=var1+var2+var3
+print(" la variable 1 es igual a", var1)
+print("la variable 2 es igual a", var2)
+print("la variable 3 es igual a", var3)
+print("el total de las tres variables es", total)
